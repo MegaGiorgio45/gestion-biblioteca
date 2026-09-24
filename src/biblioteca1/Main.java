f1f96df3f79eee40814f6e5fca3d375818bbf590
MegaGiorgio45 <lopezvaljorge3@gmail.com>
2026-09-24 19:09:31 +0200

    logica del menu

--- src/biblioteca1/Main.java
+++ src/biblioteca1/Main.java
@@ -3,7 +3,13 @@ package biblioteca1;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		LibroRepository repo = new LibroRepositoryArchivo();
+
+		menuPrincipal(sc, repo);
+
+		sc.close();
 	}
 
 	public static void menuPrincipal(Scanner sc, LibroRepository repo) {
