3470f6a0d84ec77adbc9e54a5bac268e748a0659
Guillermo <guillemdb1994@gmail.com>
2026-09-24 12:20:01 +0200

    menu

---
+++ data/biblioteca.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS biblioteca;
+
+USE biblioteca;
+
+CREATE TABLE IF NOT EXISTS libros (
+    id VARCHAR(50) PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    precio DOUBLE NOT NULL,
+    stock INT NOT NULL
+);
